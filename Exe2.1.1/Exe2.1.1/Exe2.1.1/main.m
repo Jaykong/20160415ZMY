1925159de29a3f23fe04dd0574ef82360e6e2f7d
GangsterMy <impzone@163.com>
2016-04-15 15:41:58 +0800

    Exe2.1.1

---
+++ Exe2.1.1/Exe2.1.1/Exe2.1.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exe2.1.1
+//
+//  Created by 赵梦乐 on 4/15/16.
+//  Copyright © 2016 歹徒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog (@"Programming is fun!");
+    }
+    return 0;
+}
